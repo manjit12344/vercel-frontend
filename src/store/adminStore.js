079a169f4d9ad9d88e9b8cbff352f3287455535f
manjit12344 <checkforadmin@gmail.com>
2026-05-28 21:03:17 +0530

    live backend url

--- src/store/adminStore.js
+++ src/store/adminStore.js
@@ -2,8 +2,7 @@ import { create } from "zustand";
 import axios from "axios";
 import { toast } from "react-hot-toast";
 
-const base_url = 'http://localhost:3000'
-
+const base_url = "https://vercel-backend-tau-sooty.vercel.app";
 
 export const getLoggedinUser = create((set) => ({
   users: null,
